5495c8ddf8610299dfc23b9c789dac1e6d31b48b
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-22 06:39:18 +0300

    Исправил config

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPEN_AI_KEY = os.environ["OPEN_AI_KEY"]
+OPEN_AI_KEY = os.environ.get("OPEN_AI_KEY", default="my_api_key")
 
-TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", default="my_bot_token")
 
-POSTGRES_DB = os.environ["POSTGRES_DB"]
-POSTGRES_USER = os.environ["POSTGRES_USER"]
-POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
-POSTGRES_HOST = os.environ["POSTGRES_HOST"]
-POSTGRES_PORT = os.environ["POSTGRES_PORT"]
+POSTGRES_DB = os.environ.get("POSTGRES_DB", default="my_database_name")
+POSTGRES_USER = os.environ.get("POSTGRES_USER", default="my_username")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", default="my_password")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", default="postgres")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT", default="5432")
 
 DATABASE_URL = (
     f"postgresql+asyncpg://{POSTGRES_USER}:" f"{POSTGRES_PASSWORD}@{POSTGRES_HOST}:" f"{POSTGRES_PORT}/{POSTGRES_DB}"
