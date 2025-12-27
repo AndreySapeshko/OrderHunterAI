9c7ac476989b911c12c154d387639bd41dc4f802
Andrey Sapeshko <andrdd17@gmail.com>
2025-12-27 16:34:48 +0300

    Исправления на соответствие PEP8

--- backend/app/sources/connectors/registry_connectors.py
+++ backend/app/sources/connectors/registry_connectors.py
@@ -1,4 +1,5 @@
 from backend.app.sources.connectors.kwork_projects import KworkProjectsConnector
+
 # from backend.app.sources.connectors.reddit_forhire import RedditForHireConnector
 # from backend.app.sources.connectors.telegram_channel import TelegramChannelConnector
 from backend.app.sources.registry import SourceRegistry
